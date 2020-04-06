507fcc68e347a3cb71a4b878f96597460baab15b
jackieboi <45468612+jackieboi@users.noreply.github.com>
2020-04-06 18:03:06 +0200

    Update assignment1.m

--- assignment1.m
+++ assignment1.m
@@ -20,7 +20,7 @@ syms F
 m_g = J1 + M2 * R2^2;
 c_g = c1 * R2^2 + c1 * R1^2;
 k_g = k1 * R2^2 + k2 * R1^2;
-a_g = F * R2 + M2 * g * R2;
+C_g = F * R2 + M2 * g * R2;
 
 % b)
 
@@ -36,30 +36,6 @@ omega_d = sqrt(omega_n^2 - alpha^2);
 
 % a)
 
-% syms theta(t) t
-% 
-% Parameters
-% theta0 = 0.2;
-% omega0 = 1;
-% a_g_lib = vpa(subs(a_g,F,0),3); % Moto libero
-% 
-% ODE
-% Dtheta = diff(theta,t,1);
-% Ddtheta = diff(theta,t,2);
-% 
-% ode = Ddtheta * m_g + Dtheta * c_g + theta * k_g == a_g;
-% cond1 = theta(0) == theta0;
-% cond2 = Dtheta(0) == omega0;
-% conds = [cond1, cond2];
-% theta(t) = dsolve(ode,conds);
-% theta = simplify(theta);
-% 
-% % pretty(theta)
-
-% % Plot
-
-% fplot(theta,[0,1]);
-
 lambda = roots([m_g c_g k_g]);
 
 % Condizioni iniziali
@@ -74,13 +50,33 @@ X1 = theta0 - X2;
 theta = X1*exp(lambda(1)*t) + X2*exp(lambda(2)*t);
 
 % Grafico
-
 figure
 plot(t,theta);
 axis([-inf, inf, min(theta)*1.1, max(theta)*1.1]);
 title('Time response of system free motion');
 xlabel('time [s]'); ylabel('displacement [rad]');
 grid on
+
+% Metodo alternativo usando funzioni per risolvere equazioni differenziali
+%
+% syms theta(t) t
+% 
+% % Parameters
+% theta0 = 2;
+% omega0 = 16;
+% C_g_lib = vpa(subs(C_g,F,0),3); % Moto libero
+% 
+% Dtheta = diff(theta,t,1);
+% Ddtheta = diff(theta,t,2);
+% 
+% ode = Ddtheta * m_g + Dtheta * c_g + theta * k_g == C_g_lib;
+% cond1 = theta(0) == theta0;
+% cond2 = Dtheta(0) == omega0;
+% conds = [cond1, cond2];
+% theta(t) = dsolve(ode,conds);
+% theta = simplify(theta);
+% 
+% fplot(theta,[0,10]);
 
 % b)
 
