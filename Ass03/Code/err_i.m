ee4e2fe0cbb596591ff1962e8a44c11494eaba55
emainta <emainta@users.noreply.github.com>
2020-08-08 15:44:13 +0200

    Revert "update ass 05 version 6"
    This reverts commit 5e434cfe2b9e458dae7b12e9949a4149a7cbc4cd.

--- Ass03/Code/err_i.m
+++ Ass03/Code/err_i.m
@@ -1,55 +1,41 @@
 function errore = err_i(xpar , range_H , H_exp) 
-%ERR_I Summary of this function goes here
-%   ASS05
+%ERR ASS 03
 
-% if (size(xpar,1)~= 8 || size(xpar,2)~= 1 )
-%     error('Not valid xpar');
-% end
-% 
-% if(size(range_H,1) ~= size(H_exp,1))
-%     error('Not valid lengths');
-% end
+if (length(xpar)~= 8 )
+    error('Not valid xpar');
+end
 
 csii= xpar(1);
 w0i=xpar(2);
-Aj = xpar(3);
-Bj = xpar(4);
-Cj = xpar(5);
-Dj = xpar(6);
-Ej = xpar(7);
-Fj = xpar(8);
+% [Ai,Bi,Ci,Di,Ei,Fi] = xpar(3:end); 
+Ai = xpar(3);
+Bi = xpar(4);
+Ci = xpar(5);
+Di = xpar(6);
+Ei = xpar(7);
+Fi = xpar(8);
 
+% m =1;
+% c = 2*xpar(1)*xpar(2);
+% k =  xpar(2)^2;
 
-
-% m_i =1;
-% c_i = 2*xpar(1,:).*xpar(2,:);
-% k_i =  xpar(2,:).^2;
-
-omega = 2*pi.*range_H;
-
-if(nnz(ismember(range_H,0)) ~=0)
-    omega = omega + 1e-04;
-end
+omega = 2*pi.*range_H; 
 
 %vpar=[1; 2*xpar(1)*xpar(2); xpar(2)^2; xpar(3:8)];
 %     [m;   c = 2 m w0 csi; k = w0^2 m; A;B;C;D;E;F];
 
-% Controllo su w0i e csii negative
-
-
-%m_i = 1;
-%m_i = Aj;
-
-H_anal = (Aj + 1i*Bj )./(-omega.^2 + 2*1i*omega.*(csii.*w0i) + w0i.^2) ...
-          + (Cj + 1i*Dj).*ones(size(omega)) + (Ej + 1i*Fj)./(omega.^2);
-%H_anal = (Aj)./(-omega.^2 + 2*1i*omega.*(csii.*w0i) + w0i.^2);
-% H_anal = (Aj./m_i)./(-omega.^2 + 2*1i*omega.*(csii.*w0i) + w0i.^2);
-% H_anal = (Aj)./(- m_i.*omega.^2 + 1i*c_i.*omega + k_i);
-
-% errore 
-c_e = H_exp - H_anal; %complex error
-real_e = (real(c_e));
-imag_e = (imag(c_e));
-errore = [real_e; imag_e];
+% Controllo su w0i negative
+if w0i < 0 
+    err0=1e04;
+else
+    err0 = 0;
 end
 
+
+H_anal = (Ai + 1i*Bi)./(-omega.^2 + 2i*omega.*(csii*w0i) + w0i^2) + ...
+    + (Ci+1i*Di) + (Ei+1i*Fi)./(omega.^2); %.*(ones(size(omega)))
+
+% errore 
+c_e = H_anal - H_exp; %complex error
+errore = sum(real(c_e).^2) + sum(imag(c_e).^2) + err0;
+end
--- Ass03/Code/reco.m
+++ Ass03/Code/reco.m
@@ -1,20 +1,20 @@
 function FRF_reco = reco(vparre, reinge)
 %REDO Bella
-%   RICORDA: vpar(a,b,c) = a:picc b: mis c:parametri
-    nPeaks = size(vparre,1);
-    meas = size(vparre,2);
+%   Nope
+    nPeaks = size(vparre,2);
+    meas = size(vparre,1);
     m_i = vparre(:,:,1); 
     c_i = vparre(:,:,2); 
     k_i = vparre(:,:,3); 
     Aj = vparre(:,:,4);
     Bj = vparre(:,:,5);
     omega = 2*pi.*reinge;
-    FRF_reco = zeros(length(reinge), meas);
+    FRF_reco = zeros(meas, length(reinge));
     
     for mm = 1:meas
         for pp = 1:nPeaks
-            FRF_reco(:, mm) = FRF_reco(: , mm) + ...
-                (Aj(pp,mm) + 1i*Bj(pp,mm))./(- m_i(pp,mm).*omega.^2 + 1i*c_i(pp,mm).*omega + k_i(pp,mm));
+            FRF_reco(mm,:) = FRF_reco(mm,:) + ...
+                (Aj(mm,pp) + 1i*Bj(mm,pp))./(- m_i(mm,pp).*omega.^2 + 1i*c_i(mm,pp).*omega + k_i(mm,pp));
         end
     end
     
